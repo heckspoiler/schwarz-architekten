97fea669bea63cec3f08e627548682355bf830f3
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-04-07 21:54:22 +0200

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,33 +16,35 @@ export default async function Page() {
   return (
     <Bounded>
       {/* <SliceZone slices={page.data.slices} components={components} /> */}
-      <section className={styles.Carousel}>
-        {projects.map((project) => (
-          <section key={project.id} className={styles.SlideContainer}>
-            <TitleContainer key={project.id}>
-              <p className={styles.ProjectIndex}>
-                {project.data.project_index[0]?.text}
-              </p>
-              <div>
-                <h1>{project.data.project_title[0]?.text}</h1>
-                <p>{project.data.project_subtitle[0]?.text}</p>
-                <a href={project.url ?? '#'} className={styles.Anchor}>
-                  Mehr sehen
-                  <span>
-                    <Arrow height={8} width={8} />
-                  </span>
-                </a>
+      <section className={styles.CarouselContainer}>
+        <section className={styles.Carousel}>
+          {projects.map((project) => (
+            <section key={project.id} className={styles.SlideContainer}>
+              <TitleContainer key={project.id}>
+                <p className={styles.ProjectIndex}>
+                  {(project.data.project_index[0] as any)?.text}
+                </p>
+                <div>
+                  <h1>{(project.data.project_title[0] as any)?.text}</h1>
+                  <p>{(project.data.project_subtitle[0] as any)?.text}</p>
+                  <a href={project.url ?? '#'} className={styles.Anchor}>
+                    Mehr sehen
+                    <span>
+                      <Arrow height={8} width={8} />
+                    </span>
+                  </a>
+                </div>
+              </TitleContainer>
+              <div className={styles.ImageContainer}>
+                <PrismicNextImage
+                  field={project.data.project_main_image}
+                  key={project.id}
+                  alt={project.data.project_main_image.alt ?? ''}
+                />
               </div>
-            </TitleContainer>
-            <div className={styles.ImageContainer}>
-              <PrismicNextImage
-                field={project.data.project_main_image}
-                key={project.id}
-                alt={project.data.project_main_image.alt ?? ''}
-              />
-            </div>
-          </section>
-        ))}
+            </section>
+          ))}
+        </section>
       </section>
     </Bounded>
   );
