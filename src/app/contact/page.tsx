c52457a6bb2d0dcdb399d588dc459bfa88465bd5
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-04-16 18:42:12 +0200

    contact form UI stands

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,9 +3,11 @@ import { PrismicRichText, SliceZone } from '@prismicio/react';
 import Bounded from '@/components/containers/Bounded';
 import { createClient } from '@/prismicio';
 import { components } from '@/slices';
+import Image from 'next/image';
 import TitleContainer from '@/components/containers/project/TitleContainer';
 import styles from './Contact.module.css';
 import TextblockContainer from '@/components/containers/project/TextblockContainer';
+import mailImageSrc from 'public/icons/mail.svg';
 
 export default async function Page() {
   const client = createClient();
@@ -24,11 +26,106 @@ export default async function Page() {
             <PrismicRichText field={page.data.contact_description_upper} />
           </div>
         </TextblockContainer>
-        <section className={styles.FormSection}>
-          <section className={styles.FormContainer}></section>
-        </section>
+        <TextblockContainer>
+          <section className={styles.ContactSection}>
+            <section className={styles.FormContainer}>
+              <form className={styles.Form}>
+                <div className={styles.FormUpperSection}>
+                  <label className={styles.Label}>
+                    <span className={styles.LabelWithStar}>
+                      Vorname <span className={styles.RequiredStar}>*</span>
+                    </span>
+                    <input
+                      type="text"
+                      name="name"
+                      className={styles.Input}
+                      required
+                    />
+                  </label>
+                  <label className={styles.Label}>
+                    <span className={styles.LabelWithStar}>
+                      Nachname <span className={styles.RequiredStar}>*</span>
+                    </span>
+                    <input
+                      type="text"
+                      name="surname"
+                      className={styles.Input}
+                      required
+                    />
+                  </label>
+                </div>
+                <div className={styles.FormLowerSection}>
+                  <label className={styles.Label}>
+                    <span className={styles.LabelWithStar}>
+                      Telefonnummer{' '}
+                      <span className={styles.RequiredStar}>*</span>
+                    </span>
+                    <input
+                      type="tel"
+                      name="phone"
+                      className={styles.Input}
+                      required
+                    />
+                  </label>
+                  <label className={styles.Label}>
+                    <span className={styles.LabelWithStar}>
+                      Email <span className={styles.RequiredStar}>*</span>
+                    </span>
+                    <input
+                      type="email"
+                      name="email"
+                      className={styles.Input}
+                      required
+                    />
+                  </label>
+                </div>
+                <label className={styles.Label}>
+                  <span className={styles.LabelWithStar}>Ihre Nachricht </span>
+                  <textarea
+                    name="message"
+                    className={styles.Textarea}
+                    required
+                  />
+                </label>
+                <button type="submit" className={styles.Button}>
+                  Senden
+                </button>
+              </form>
+            </section>
+            <section className={styles.AddressSection}>
+              <h3>Kontakt</h3>
+              <div className={styles.Address}>
+                <h4>Dietrich Schwarz Architekten AG</h4>
+                <p>Seefeldstrasse 224</p>
+                <p>CH-8008 Zürich</p>
+              </div>
+              <div className={styles.Contact}>
+                <div className={styles.Email}>
+                  <Image
+                    src="/icons/phone.svg"
+                    width={18}
+                    height={18}
+                    alt="Phone Symbol"
+                  />
+                  <a href="tel:+41442123456">+41 44 212 34 56</a>
+                </div>
+                <div className={styles.Email}>
+                  <Image
+                    src="/icons/email.svg"
+                    width={18}
+                    height={18}
+                    alt="Email Symbol"
+                  />
+                  <a href="mailto: info@schwarz-architekten.com">
+                    info@schwarz-architekten.com
+                  </a>
+                </div>
+              </div>
+            </section>
+          </section>
+        </TextblockContainer>
       </section>
-      <SliceZone slices={page.data.slices} components={components} />;
+      <SliceZone slices={page.data.slices} components={components} />
     </Bounded>
   );
 }
