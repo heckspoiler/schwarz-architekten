6676cb4d87247f93ce755409194f9ad3dea5d505
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-04-28 13:39:32 +0200

    bounded animation added

--- src/components/containers/Bounded.tsx
+++ src/components/containers/Bounded.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
 import styles from './Bounded.module.css';
+import { usePathname } from 'next/navigation';
+import gsap from 'gsap';
 
 type BoundedProps = {
   as?: React.ElementType;
@@ -14,8 +17,34 @@ export default function Bounded({
   children,
   ...restProps
 }: BoundedProps) {
+  const pathname = usePathname();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+
+    if (ref.current) {
+      tl.to(ref.current, {
+        duration: 0.3,
+        autoAlpha: 0,
+        blur: 5,
+        y: 20,
+        onComplete: () => {
+          gsap.fromTo(
+            ref.current,
+            { autoAlpha: 0, y: 20 },
+            {
+              duration: 0.3,
+              autoAlpha: 1,
+              y: 0,
+            }
+          );
+        },
+      });
+    }
+  }, [pathname]);
   return (
-    <Comp className={styles.Main} {...restProps}>
+    <Comp className={styles.Main} {...restProps} ref={ref}>
       {children}
     </Comp>
   );
